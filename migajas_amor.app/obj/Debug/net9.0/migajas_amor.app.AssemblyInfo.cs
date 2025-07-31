e7af6f2d387458e3de3fdff890b51e637490f224
Andrea Chavez <nc24001@itcha.edu.sv>
2025-07-31 10:02:24 -0600

    ROJO

--- migajas_amor.app/obj/Debug/net9.0/migajas_amor.app.AssemblyInfo.cs
+++ migajas_amor.app/obj/Debug/net9.0/migajas_amor.app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("migajas_amor.app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f66a844f8e0fb1f8aaac046d2e6ae4226ea1ae1d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92542050706d0679a0eed7cc1b12fbe31d782f93")]
 [assembly: System.Reflection.AssemblyProductAttribute("migajas_amor.app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("migajas_amor.app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
